10677f590bf741183ce415b9f553b824a731d4ee
Mana007777 <e7ifubbb@gmail.com>
2026-06-18 14:24:27 +0300

    creating Sales controller

---
+++ app/Http/Controllers/SalesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SalesController extends Controller
+{
+    public function store(Request $request)
+    {
+        $file = $request->file('file');
+        $data = array_map('str_getcsv', file($file));
+
+        return response()->json(['message' => 'File uploaded successfully', 'data' => $data]);
+    }
+}
