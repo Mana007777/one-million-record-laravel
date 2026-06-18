1ce5d16c16fb2592df7ca8036f8d77bc88bb1328
Mana007777 <e7ifubbb@gmail.com>
2026-06-18 14:58:00 +0300

    Storing inside the File

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -18,6 +18,15 @@ class SalesController extends Controller
             $data = array_map('str_getcsv', file($request->file));
             $header = $data[0];
             unset($data[0]);
+
+            $chunks = array_chunk($data, 1000);
+
+            foreach ($chunks as $key => $chunk){
+                $name = "tmp${$key}.csv";
+                $path = resource_path("temp");
+                file_put_contents("$path . $name",$chunk);
+            }
+
             foreach ($data as $item) {
                 $saleData = array_combine($header, $item);
                 Sales::create($saleData);
